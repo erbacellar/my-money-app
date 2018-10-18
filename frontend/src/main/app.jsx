1d85b4ddb27e11dbe6760a71dcda7635c1d14f68
Ernane Jose Ribas Bacellar <ernaneribas92@gmail.com>
2018-10-18 13:48:49 -0300

    Finalizacao do projeto com autenticacao

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -1,4 +1,3 @@
-import '../common/template/dependencies'
 import React from 'react'
 import Header from '../common/template/header'
 import SideBar from '../common/template/sideBar'
@@ -12,7 +11,7 @@ export default props => (
         <Header />
         <SideBar />
         <div className='content-wrapper'>
-            <Routes />
+            {props.children}
         </div>
         <Footer />
         <Messages />
